1d8ad1c207d72f85b9472c3d5ca444c5929efd7b
Masaharu Shimizu <masaharu.shimizu08@gmail.com>
2022-06-04 17:20:18 +0900

    firebaseの設定を環境変数から読み込む設定を追加

---
+++ src/env.d.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+interface ImportMetaEnv {
+  readonly VITE_API_KEY?: string
+  readonly VITE_AUTH_DOMAIN?: string
+  readonly VITE_PROJECT_ID?: string
+  readonly VITE_MESSAGING_SENDER_ID?: string
+  readonly VITE_APP_ID?: string
+  readonly VITE_MEASUREMENT_ID?: string
+}
---
+++ src/lib/firebase.ts
@@ -0,0 +1,4 @@
+import * as firebase from 'firebase/app'
+import { firebaseConfig as config } from '@/config'
+
+firebase.initializeApp(config)
