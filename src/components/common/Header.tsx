3c5a365ea1f73896bc1f351eec835852b443bbe5
Masaharu Shimizu <masaharu.shimizu08@gmail.com>
2022-06-08 21:56:11 +0900

    HeaderにSocialアイコンを追加

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,10 +1,32 @@
 export const Header = () => {
   return (
     <>
-      <div className="navbar flex justify-center items-start">
-        <div className="flex flex-col">
+      <div className="navbar flex justify-between items-center px-3">
+        <div className="flex flex-col items-start">
           <p className="text-5xl leading-none">Mybits</p>
           <p className="text-base leading-none text-gray-500">Catchcopy here</p>
+        </div>
+        <div>
+          <div>
+            <figure className="w-7 h-7">
+              <a href="#">
+                <img
+                  src="https://storage.googleapis.com/mybits-test/opensea-icon-gray.svg"
+                  alt="opensea"
+                />
+              </a>
+            </figure>
+          </div>
+          <div className="pl-3 align-middle">
+            <figure className="w-7 h-7 align-middle">
+              <a href="#">
+                <img
+                  src="https://storage.googleapis.com/mybits-test/twitter-icon-gray.svg"
+                  alt="twitter"
+                />
+              </a>
+            </figure>
+          </div>
         </div>
       </div>
     </>
