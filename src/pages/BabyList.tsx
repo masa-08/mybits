755b2c6d27866707b62b5e370f34c3cd2652a8e1
Masaharu Shimizu <masaharu.shimizu08@gmail.com>
2022-06-15 08:31:15 +0900

    NFT一覧画面にデスクトップ版のUIを追加

--- src/pages/BabyList.tsx
+++ src/pages/BabyList.tsx
@@ -1,15 +1,24 @@
-import { Filter, List, ListItem } from '@/components/BabyList'
+import { Filter, List, ListItem, ListItemMobile } from '@/components/BabyList'
 import { dummy } from '@/store'
 
 export const BabyList = () => {
   return (
     <>
       <Filter />
-      <List>
-        {dummy.map((d) => {
-          return <ListItem key={d.id} path={d.path} id={d.id} address={d.address} />
-        })}
-      </List>
+      <div className="hidden md:block">
+        <List>
+          {dummy.map((d) => {
+            return <ListItem key={d.id} path={d.path} id={d.id} address={d.address} />
+          })}
+        </List>
+      </div>
+      <div className="block md:hidden">
+        <List>
+          {dummy.map((d) => {
+            return <ListItemMobile key={d.id} path={d.path} id={d.id} address={d.address} />
+          })}
+        </List>
+      </div>
     </>
   )
 }
