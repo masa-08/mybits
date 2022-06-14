263be696a49fb203aacdcbd83c5b345d64f298bb
Masaharu Shimizu <masaharu.shimizu08@gmail.com>
2022-06-15 08:32:02 +0900

    NFT詳細画面にデスクトップUIを追加

---
+++ src/components/BabyDetail/Carousel.tsx
@@ -0,0 +1,23 @@
+import { Image } from './Image'
+import { dummy } from '@/store'
+
+export const Carousel = () => {
+  const data = dummy[0]
+  return (
+    <>
+      {
+        // ダミー実装なので、実データを扱う際に修正する
+      }
+      <p className="text-3xl px-3 mb-5">Would u like to see other babies?</p>
+      <div className="carousel carousel-center space-x-4 rounded-box">
+        <button className="btn btn-circle bg-gray-300 border-gray-300 w-9 h-44">{'<'}</button>
+        <Image src={data?.path || ''} />
+        <Image src={data?.path || ''} />
+        <Image src={data?.path || ''} />
+        <Image src={data?.path || ''} />
+        <Image src={data?.path || ''} />
+        <button className="btn btn-circle bg-gray-300 border-gray-300 w-9 h-44">{'>'}</button>
+      </div>
+    </>
+  )
+}
--- src/components/BabyDetail/Description.tsx
+++ src/components/BabyDetail/Description.tsx
@@ -7,10 +7,14 @@ type Props = {
 export const Description: React.FC<Props> = ({ id = 'unasigned' }) => {
   return (
     <>
-      <p className="text-3xl leading-none">#{id}</p>
-      <p className="text-4xl text-gray-400">Unknown</p>
-      <p className="text-xl mt-3">Status: Waiting 4 u 2 adopt me!</p>
-      <p className="text-xl">Owner : Will u b my owner?</p>
+      <div>
+        <p className="text-6xl leading-none">#{id}</p>
+        <p className="text-7xl text-gray-400">Unknown</p>
+      </div>
+      <div>
+        <p className="text-3xl mt-3">Status: Waiting 4 u 2 adopt me!</p>
+        <p className="text-3xl">Owner : Will u b my owner?</p>
+      </div>
     </>
   )
 }
