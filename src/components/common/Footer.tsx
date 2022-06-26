b1e198eaa447de922c281c14f3650d7edf54012c
Masaharu Shimizu <masaharu.shimizu08@gmail.com>
2022-06-26 10:44:50 +0900

    レスポンシブ対応のためにデスクトップのみ適用されるFrameを用意

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -3,24 +3,26 @@ import { OpenseaIcon, TwitterIcon } from './Icons'
 export const Footer = () => {
   return (
     <>
-      <div className="p-3 pl-6 mt-12 bg-gray-300">
-        <div className="max-w-5xl mx-auto">
-          <div className="flex justify-between items-center">
-            <div className="flex divide-gray-100 divide-x">
-              <a href="#" className="flex items-center gap-2 pr-2" style={{ color: '#2081E2' }}>
-                <figure className="w-5 h-5">
-                  <OpenseaIcon />
-                </figure>
-                OpenSea
-              </a>
-              <a href="#" className="flex items-center gap-2 pl-2" style={{ color: '#1b9df0' }}>
-                <figure className="w-5 h-5">
-                  <TwitterIcon />
-                </figure>
-                Twitter
-              </a>
+      <div className="hidden md:block">
+        <div className="p-3 pl-6 mt-12 bg-gray-300">
+          <div className="max-w-5xl mx-auto">
+            <div className="flex justify-between items-center">
+              <div className="flex divide-gray-100 divide-x">
+                <a href="#" className="flex items-center gap-2 pr-2" style={{ color: '#2081E2' }}>
+                  <figure className="w-5 h-5">
+                    <OpenseaIcon />
+                  </figure>
+                  OpenSea
+                </a>
+                <a href="#" className="flex items-center gap-2 pl-2" style={{ color: '#1b9df0' }}>
+                  <figure className="w-5 h-5">
+                    <TwitterIcon />
+                  </figure>
+                  Twitter
+                </a>
+              </div>
+              <p className="text-sm text-gray-400">&copy; 2022 NEOBABY</p>
             </div>
-            <p className="text-sm text-gray-400">&copy; 2022 NEOBABY</p>
           </div>
         </div>
       </div>
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export { Top } from './Top'
 export { BabyList } from './BabyList'
 export { BabyDetail } from './BabyDetail'
+export { Frame } from './Frame'
