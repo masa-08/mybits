a808912ce1426c7812fca8cbe75d3a40373d188b
Masaharu Shimizu <masaharu.shimizu08@gmail.com>
2022-06-26 10:33:49 +0900

    モバイル版のロードマップを追加

---
+++ src/components/Top/RoadmapItems.tsx
@@ -0,0 +1,12 @@
+import { RoadmapItem } from './RoadmapItem'
+
+export const RoadmapItems = () => {
+  return (
+    <>
+      <RoadmapItem title="#1 Show Owner" body="Show ownership in each baby's page" />
+      <RoadmapItem title="#2 Name Baby" body="Allow the owner to name the baby" />
+      <RoadmapItem title="#3 Take Care" body="The stats will change with the care!" />
+      <RoadmapItem title="#4 Generative" body="New babies to be generated!" />
+    </>
+  )
+}
