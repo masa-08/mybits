02503cf40cddf2bc3e7e609fc19a63c563ef1197
Masaharu Shimizu <masaharu.shimizu08@gmail.com>
2022-07-01 11:49:08 +0900

    ロードマップのデザインを変更

--- src/components/Top/Roadmap.tsx
+++ src/components/Top/Roadmap.tsx
@@ -6,7 +6,7 @@ export const Roadmap = () => {
       <div className="hidden md:block">
         <p className="text-5xl leading-none px-3 mb-4">Roadmap</p>
         <div className="flex gap-3 justify-between">
-          <RoadmapItems />
+          <RoadmapItems divider={<></>} />
         </div>
       </div>
     </>
