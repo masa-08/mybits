923e0f97241e98f9c1f94ffa034e1c818df9d92d
Masaharu Shimizu <masaharu.shimizu08@gmail.com>
2022-07-01 11:48:32 +0900

    Aboutのデザインを変更
    Aboutのアイコン下のパディングを調整
    
    Heartのサイズを調整

--- src/components/Top/AboutMobile.tsx
+++ src/components/Top/AboutMobile.tsx
@@ -5,8 +5,8 @@ export const AboutMobile = () => {
     <>
       <div className="md:hidden">
         <div className="p-3 flex flex-col gap-4 mb-4">
-          <p className="text-5xl leading-none">About NEOBABY</p>
-          <p className="text-2xl leading-none">
+          <p className="text-5xl">About NEOBABY</p>
+          <p className="text-xl font-fira">
             NEOBABY is a NFT art collection on Ethereum, and you can buy it from OpenSea.
           </p>
         </div>
