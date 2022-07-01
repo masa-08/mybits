a4396a0be7c13a35d71d62f9535461874d5a5996
Masaharu Shimizu <masaharu.shimizu08@gmail.com>
2022-07-01 11:45:10 +0900

    一覧画面のカードデザインを変更

--- src/components/BabyList/ListItem.tsx
+++ src/components/BabyList/ListItem.tsx
@@ -10,10 +10,10 @@ type Props = {
 export const ListItem: React.FC<Props> = ({ path, id, address = 'Waiting 4 U' }) => {
   return (
     <>
-      <div className="card bg-base-100 rounded-none" style={{ width: 'calc((100% - 2.25rem)/4)' }}>
+      <div className="card bg-base-100 rounded-xl" style={{ width: 'calc((100% - 2.25rem)/4)' }}>
         <Link to={id}>
           <figure>
-            <img src={path} alt="image" className="rounded-xl" />
+            <img src={path} alt="image" className="rounded-t-xl" />
           </figure>
           <div className="card-body px-3 py-2">
             <p className="text-xl text-gray-500" style={{ lineHeight: 0.5 }}>
