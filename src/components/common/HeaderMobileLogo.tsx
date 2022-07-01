7b04c762218f77abb1626027631c63761b77b633
Masaharu Shimizu <masaharu.shimizu08@gmail.com>
2022-07-01 11:50:30 +0900

    ヘッダーのデザインを変更

--- src/components/common/HeaderMobileLogo.tsx
+++ src/components/common/HeaderMobileLogo.tsx
@@ -11,7 +11,6 @@ export const HeaderMobileLogo: React.FC<Props> = ({ top = false }) => {
       <div className="flex flex-col items-start">
         <Link to="/">
           <p className={top ? 'title-mobile-top' : 'title-mobile'}>NEOBABY</p>
-          <p className={top ? 'subtitle-mobile-top' : 'subtitle-mobile'}>Lovable NFT buddy</p>
         </Link>
       </div>
     </>
