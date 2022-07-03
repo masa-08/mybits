788a7167d2c7d3da708d216e068851a40f680946
Masaharu Shimizu <masaharu.shimizu08@gmail.com>
2022-07-03 11:28:05 +0900

    ギャラリーのリンク有効化とバラエティ化

--- src/components/Top/GalleryMobile.tsx
+++ src/components/Top/GalleryMobile.tsx
@@ -8,14 +8,22 @@ export const GalleryMobile = () => {
         <p className="text-5xl px-3 mb-4">Gallery</p>
         <div className="flex gap-3">
           <figure style={{ flex: '0 0 calc(66.6% - 4px)' }}>
-            <img src={dummy[0].path} alt="image" className="rounded-2xl" />
+            <Link to="/babies/001">
+              <img src={dummy[0].path} alt="image" className="rounded-xl" />
+            </Link>
           </figure>
           <div className="flex flex-col gap-3">
             <figure>
-              <img src={dummy[0].path} alt="image" className="rounded-2xl" />
+              <Link to="/babies/002">
+                <img src={dummy[1].path} alt="image" className="rounded-xl" />
+              </Link>
             </figure>
             <figure>
-              <img src={dummy[0].path} alt="image" className="rounded-2xl" />
+              <img
+                src="https://storage.googleapis.com/mybits-test/baby-silhouette.webp"
+                alt="image"
+                className="rounded-xl bg-yellow-300 p-3"
+              />
             </figure>
           </div>
         </div>
@@ -23,14 +31,22 @@ export const GalleryMobile = () => {
         <div className="flex gap-3">
           <div className="flex flex-col gap-3">
             <figure>
-              <img src={dummy[0].path} alt="image" className="rounded-2xl" />
+              <Link to="/babies/003">
+                <img src={dummy[2].path} alt="image" className="rounded-xl" />
+              </Link>
             </figure>
             <figure>
-              <img src={dummy[0].path} alt="image" className="rounded-2xl" />
+              <img
+                src="https://storage.googleapis.com/mybits-test/baby-silhouette.webp"
+                alt="image"
+                className="rounded-xl bg-green-400 p-3"
+              />
             </figure>
           </div>
           <figure style={{ flex: '0 0 calc(66.6% - 4px)' }}>
-            <img src={dummy[0].path} alt="image" className="rounded-2xl" />
+            <Link to="/babies/004">
+              <img src={dummy[3].path} alt="image" className="rounded-xl" />
+            </Link>
           </figure>
         </div>
         <div className="pt-3">
